879235603e520c8ccb28679af0f9a35dcdc94e5c
crynobone <crynobone@gmail.com>
2013-10-12 13:07:56 +0800

    Defer the CommandServiceProvider
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Auth/CommandServiceProvider.php
+++ src/Orchestra/Auth/CommandServiceProvider.php
@@ -5,6 +5,13 @@ use Illuminate\Support\ServiceProvider;
 class CommandServiceProvider extends ServiceProvider {
 
 	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
 	 * Register the service provider.
 	 *
 	 * @return void
@@ -17,5 +24,15 @@ class CommandServiceProvider extends ServiceProvider {
 		});
 
 		$this->commands('orchestra.commands.auth');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('orchestra.commands.auth');
 	}
 }
