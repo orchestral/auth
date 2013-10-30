3861882390853e146181c72cda2a9a89bf3c8d1b
Mior Muhammad Zaki <crynobone@gmail.com>
2013-10-31 00:59:08 +0800

    Update service provider to use bindShared.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Orchestra/Auth/AuthServiceProvider.php
+++ src/Orchestra/Auth/AuthServiceProvider.php
@@ -22,7 +22,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected function registerAuth()
     {
-        $this->app['auth'] = $this->app->share(function ($app) {
+        $this->app->bindShared('auth', function ($app) {
             // Once the authentication service has actually been requested by the developer
             // we will set a variable in the application indicating such. This helps us
             // know that we need to set any queued cookies in the after event later.
@@ -39,7 +39,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected function registerAcl()
     {
-        $this->app['orchestra.acl'] = $this->app->share(function ($app) {
+        $this->app->bindShared('orchestra.acl', function ($app) {
             return new Acl\Environment($app['auth']->driver());
         });
 
