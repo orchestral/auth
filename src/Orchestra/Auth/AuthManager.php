6aafc2424f007a76e45069cae8f89e5cbf4269c3
crynobone <crynobone@gmail.com>
2014-01-19 13:08:50 +0800

    Fix indentation.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Auth/AuthManager.php
+++ src/Orchestra/Auth/AuthManager.php
@@ -2,18 +2,18 @@
 
 class AuthManager extends \Illuminate\Auth\AuthManager
 {
-	/**
+    /**
      * {@inheritdoc}
-	 */
-	protected function callCustomCreator($driver)
-	{
-		$custom = parent::callCustomCreator($driver);
+     */
+    protected function callCustomCreator($driver)
+    {
+        $custom = parent::callCustomCreator($driver);
 
         if ($custom instanceof Guard) {
             return $custom;
         }
 
-		return new Guard($custom, $this->app['session.store']);
+        return new Guard($custom, $this->app['session.store']);
     }
 
     /**
