3f8d93becbefb89cbd832aebaff7d1c1376ca22a
crynobone <crynobone@gmail.com>
2014-11-02 16:23:54 +0800

    Remove code which has been implemented upstream.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Auth/AuthManager.php
+++ src/Auth/AuthManager.php
@@ -5,20 +5,6 @@ class AuthManager extends \Illuminate\Auth\AuthManager
     /**
      * {@inheritdoc}
      */
-    protected function callCustomCreator($driver)
-    {
-        $custom = parent::callCustomCreator($driver);
-
-        if ($custom instanceof Guard) {
-            return $custom;
-        }
-
-        return new Guard($custom, $this->app['session.store']);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function createDatabaseDriver()
     {
         $provider = $this->createDatabaseProvider();
