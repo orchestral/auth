c36b782a4ab3a860e6cc4189cf0acc9858cd1b63
crynobone <crynobone@gmail.com>
2015-01-23 07:42:33 +0800

    Add shebang.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 git subsplit init git@github.com:orchestral/auth.git
 git subsplit publish --heads="master" --no-tags src/Authorization:git@github.com:orchestral/authorization.git
 rm -rf .subsplit/
--- build/subsplit-full.sh
+++ build/subsplit-full.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 git subsplit init git@github.com:orchestral/auth.git
 git subsplit publish --heads="master" src/Authorization:git@github.com:orchestral/authorization.git
 rm -rf .subsplit/
