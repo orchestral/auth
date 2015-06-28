71a9a00a2196a27ac8253895a379859585a2d25d
crynobone <crynobone@gmail.com>
2015-06-29 07:01:14 +0800

    Typehint to Application contract.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Auth/AuthServiceProvider.php
+++ src/Auth/AuthServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Orchestra\Auth;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Auth\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected function registerAuthenticator()
     {
-        $this->app->singleton('auth', function ($app) {
+        $this->app->singleton('auth', function (Application $app) {
             // Once the authentication service has actually been requested by the developer
             // we will set a variable in the application indicating such. This helps us
             // know that we need to set any queued cookies in the after event later.
@@ -20,7 +21,7 @@ class AuthServiceProvider extends ServiceProvider
             return new AuthManager($app);
         });
 
-        $this->app->singleton('auth.driver', function ($app) {
+        $this->app->singleton('auth.driver', function (Application $app) {
             return $app->make('auth')->driver();
         });
     }
