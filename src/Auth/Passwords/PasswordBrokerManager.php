b2994f60d7894cfdeb4a0296182f0e1cc44973c3
crynobone <crynobone@gmail.com>
2015-12-04 09:25:21 +0800

    Update password broker.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Auth/Passwords/PasswordBrokerManager.php
@@ -0,0 +1,31 @@
+<?php namespace Orchestra\Auth\Passwords;
+
+use Illuminate\Auth\Passwords\PasswordBrokerManager as BaseManager;
+
+class PasswordBrokerManager extends BaseManager
+{
+    /**
+     * Resolve the given broker.
+     *
+     * @param  string  $name
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    protected function resolve($name)
+    {
+        $config = $this->getConfig($name);
+
+        if (is_null($config)) {
+            throw new InvalidArgumentException("Password resetter [{$name}] is not defined.");
+        }
+
+        // The password broker uses a token repository to validate tokens and send user
+        // password e-mails, as well as validating that password reset process as an
+        // aggregate service of sorts providing a convenient interface for resets.
+        return new PasswordBroker(
+            $this->createTokenRepository($config),
+            $this->createUserProvider($config['provider']),
+            $this->app->make('orchestra.notifier')->driver(),
+            $config['email']
+        );
+    }
+}
