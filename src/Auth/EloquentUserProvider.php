ebb8ed21af4e3171ba4b7c9925a729ea5271bea3
crynobone <crynobone@gmail.com>
2017-09-25 09:32:59 +0800

    Fixes `remember_me` Timing Attack Vector
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Auth/EloquentUserProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Orchestra\Auth;
+
+use Illuminate\Auth\EloquentUserProvider as UserProvider;
+
+class EloquentUserProvider extends UserProvider
+{
+    /**
+     * Retrieve a user by their unique identifier and "remember me" token.
+     *
+     * @param  mixed  $identifier
+     * @param  string  $token
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveByToken($identifier, $token)
+    {
+        $model = $this->createModel();
+        $model = $model->newQuery()
+                    ->where($model->getAuthIdentifierName(), $identifier)
+                    ->first();
+
+        if (! $model) {
+            return;
+        }
+
+        $rememberToken = $model->getRememberToken();
+
+        return $rememberToken && hash_equals($rememberToken, $token) ? $model : null;
+    }
+}
