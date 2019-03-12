a5d41920579057f9d2b00b7002155ec5cff0d699
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-12 15:02:27 +0800

    Move migrations folder.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2013_04_12_012833_orchestra_auth_create_user_meta_table.php
+++ database/migrations/2013_04_12_012833_orchestra_auth_create_user_meta_table.php
@@ -13,17 +13,7 @@ class OrchestraAuthCreateUserMetaTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_meta', function (Blueprint $table) {
-            $table->bigIncrements('id');
-
-            $table->bigInteger('user_id')->unsigned();
-            $table->string('name', 255);
-            $table->longText('value');
-
-            $table->nullableTimestamps();
-
-            $table->unique(['user_id', 'name']);
-        });
+        //
     }
 
     /**
