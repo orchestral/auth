d8533f2c600bd7529f86b1d40c99a52f39444d16
Mior Muhammad Zaki <crynobone@gmail.com>
2020-08-30 15:37:42 +0800

    Update tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Auth/Feature/TestCase.php
+++ tests/Auth/Feature/TestCase.php
@@ -7,16 +7,6 @@ use Orchestra\Testbench\TestCase as Testbench;
 abstract class TestCase extends Testbench
 {
     /**
-     * Setup the test environment.
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->withFactories(__DIR__.'/../../factories');
-    }
-
-    /**
      * Override application aliases.
      *
      * @param  \Illuminate\Foundation\Application  $app
