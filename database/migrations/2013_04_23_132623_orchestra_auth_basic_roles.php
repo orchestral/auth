218c2db65d6f595752ee012b317dd7c8d6504f61
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-18 03:29:49 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2013_04_23_132623_orchestra_auth_basic_roles.php
+++ database/migrations/2013_04_23_132623_orchestra_auth_basic_roles.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
