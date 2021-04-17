a3a43919a2390978ecabc0cde5aac2f144d52a77
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-18 03:26:39 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Auth/AuthServiceProvider.php
+++ src/Auth/AuthServiceProvider.php
@@ -41,11 +41,6 @@ class AuthServiceProvider extends ServiceProvider
     protected function registerAuthenticator(): void
     {
         $this->app->singleton('auth', static function (Container $app) {
-            // Once the authentication service has actually been requested by the developer
-            // we will set a variable in the application indicating such. This helps us
-            // know that we need to set any queued cookies in the after event later.
-            $app['auth.loaded'] = true;
-
             return new AuthManager($app);
         });
 
